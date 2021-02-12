b91745dc3e456670bc11a28cd91c7d919c93481e
Rachael Aptowitz <raptowitz@gmail.com>
2021-02-11 23:06:50 -0800

    recursive insert_at(value, index) method

--- linked_lists.rb
+++ linked_lists.rb
@@ -39,14 +39,16 @@ class LinkedList
     size(node.next_node, count)
   end
 
+  # returns the node at the given index
   def at(index, current_index = 0, node = @head)
-    return 'Not a valid index' if index >= size
+    return 'Not a valid index' if index >= size || index.negative?
     return node if index == current_index
 
     current_index += 1
     at(index, current_index, node.next_node)
   end
 
+  # removes the last element from the list
   def pop(node = @head)
     if node.next_node.next_node.nil?
       node.next_node = nil
@@ -56,6 +58,7 @@ class LinkedList
     pop(node.next_node)
   end
 
+  # returns true if the passed in value is in the list and otherwise returns false
   def contains?(value, node = @head)
     return false if node.nil?
     return true if node.value == value
@@ -63,6 +66,7 @@ class LinkedList
     contains?(value, node.next_node)
   end
 
+  # returns the index of the node containing value, or nil if not found
   def find(value, current_index = 0, node = @head)
     return nil if node.nil?
     return current_index if node.value == value
@@ -71,12 +75,29 @@ class LinkedList
     find(value, current_index, node.next_node)
   end
 
+  # represent your LinkedList objects as strings
   def to_s(node = @head, string = [])
     return string << 'nil' if node.nil?
 
     string << node.value
     to_s(node.next_node, string)
     string.join('->')
+  end
+
+  # inserts the node with the provided value at the given index
+  def insert_at(value, index, current_index = 0, node = @head)
+    return 'Not a valid index' if index > size || index.negative?
+    return append(value) if index == size
+    return prepend(value) if index.zero?
+
+    if current_index + 1 == index
+      new_node = Node.new(value)
+      new_node.next_node = node.next_node
+      node.next_node = new_node
+      return
+    end
+    current_index += 1
+    insert_at(value, index, current_index, node.next_node)
   end
 end
 
@@ -94,5 +115,5 @@ list.append(5)
 list.append(4)
 list.append(3)
 list.append(2)
-p list.to_s
-p list
+puts list.insert_at(6,8)
+puts list.to_s
