94c5c6d596dc51616e9641f05df8e5e73b35fd97
Rachael Aptowitz <raptowitz@gmail.com>
2021-03-06 12:57:15 -0800

    reduce brand size of knight moves

--- knights_travails.rb
+++ knights_travails.rb
@@ -10,12 +10,6 @@ class Board
     @board = Array.new(8) { [1, 2, 3, 4, 5, 6, 7, 8] }
   end
 
-  def print_board
-    @board.each do |row|
-      puts row.join
-    end
-  end
-
   def create_spaces
     @spaces = []
     @board.each_with_index do |row, y_cord|
@@ -35,23 +29,17 @@ class Knight
   end
 
   def knight_moves
-    level = { @start => 0 }
     parent = {}
-    i = 1
     frontier = [@start]
-    until level.keys.include?(@finish)
+    until parent.keys.include?(@finish)
       until frontier[0].nil?
         queue = available_moves(frontier[0])
         queue.each_entry do |next_move|
-          if level.keys.none?(next_move)
-            level[next_move] = i
-            parent[next_move] = frontier[0]
-          end
+          parent[next_move] = frontier[0] if parent.keys.none?(next_move)
         end
         frontier.shift
       end
       frontier = queue
-      i += 1
     end
     print_path(parent)
   end
